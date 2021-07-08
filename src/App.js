5a8bc6efd54458e19efee5ac0c73881104dee972
Stas <stazzzdzr@gmail.com>
2021-07-08 10:17:56 +0300

    HW is ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+let text = 'test variable';
+
+const style = {
+	dontSize: '24px',
+	fontStyle: 'italic',
+	color: 'red',
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let text2 = '222';
+	return (
+		<>
+			<div className="container">
+				<h1 style={style}>app_1</h1>
+				<img src="/images/clone-logo.png" alt="react-logo"/>
+				<ul>
+					<li>Hello</li>
+					<li>{text+text2}</li>
+				</ul>
+			</div>
+		</>
+	);
 }
 
 export default App;
